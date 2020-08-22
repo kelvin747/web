d9561d68a1e287d2bc508194b19e624bc9c8df1d
kelvin747 <62883366+kelvin747@users.noreply.github.com>
2020-08-22 14:46:09 +0300

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,5 @@
+import pyautogui
+pyautogui.alert('This is a message.','Important')
+pyautogui.confirm('Do you want to continue') #click cancel
+pyautogui.prompt('What is your name')
+pyautogui.password('What is the password?')
